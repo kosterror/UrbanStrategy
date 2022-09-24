2674e08f66307f03ceaaea2c8c96647320a85b0a
kosterror <samoylenko.konstantin.3197@gmail.com>
2022-09-24 19:29:36 +0700

    Removed extra

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,5 @@
-import enumeration.BuildingType;
-import utility.Pair;
-
 public class Main {
     public static void main(String[] args) {
-
-        GameController gameController = new GameController();
-
-        gameController.createBuilding(BuildingType.FACTORY, new Pair<Integer>(1, 1));
-        gameController.createBuilding(BuildingType.HOUSE, new Pair<Integer>(-1, 2));
-        gameController.createBuilding(BuildingType.MINE, new Pair<Integer>(2, 3));
-        gameController.createBuilding(BuildingType.STADIUM, new Pair<Integer>(3, 4));
-        gameController.createBuilding(BuildingType.FARM, new Pair<Integer>(3, 4));
-        gameController.createBuilding(BuildingType.FACTORY, new Pair<Integer>(3, 4));
-        gameController.createBuilding(BuildingType.PARK, new Pair<Integer>(3, 4));
-        gameController.createBuilding(BuildingType.FARM, new Pair<Integer>(3, 4));
-        gameController.createBuilding(BuildingType.FACTORY, new Pair<Integer>(3, 4));
-
 
     }
 }
