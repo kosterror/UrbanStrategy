62102b25168c6d78de7aa04cab35c555813c910c
kosterror <samoylenko.konstantin.3197@gmail.com>
2022-09-24 18:14:04 +0700

    Moved list of buildings into city

--- src/GameController.java
+++ src/GameController.java
@@ -3,33 +3,29 @@ import city.City;
 import enumeration.BuildingType;
 import utility.Pair;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class GameController {
 
     private final BuildingFactory buildingFactory;
     private final City city;
     private final Observer observer;
-    private final List<Building> buildingList;
 
     public GameController() {
         buildingFactory = new BuildingFactory();
         city = new City();
         observer = new Observer();
-        buildingList = new ArrayList<>();
     }
 
     public Building createBuilding(BuildingType type, Pair<Integer> coordinates) {
         Building newBuilding =  buildingFactory.create(type, city, coordinates);
-        buildingList.add(newBuilding);
+
+        city.addBuilding(newBuilding);
         observer.registerObservable(newBuilding);
 
         return newBuilding;
     }
 
     public void destroyBuilding(Building building){
-        buildingList.remove(building);
+        city.removeBuilding(building);
         observer.unregisterObservable(building);
     }
 
--- src/city/City.java
+++ src/city/City.java
@@ -1,26 +1,33 @@
 package city;
 
+import buildings.Building;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class City {
 
     private int money;
     private int resources;
     private int food;
     private int happiness;
+    private final List<Building> buildingList;
 
     public City() {
         money = 0;
         resources = 0;
         food = 0;
         happiness = 0;
+        buildingList = new ArrayList<>();
     }
 
     public void increaseStats(int money, int resources, int food, int happiness) {
         float coefficient = this.happiness / 100F;
 
-        this.money = (money>0)?this.money + (int) (money * coefficient):this.money + (int) (money / coefficient);
-        this.resources = (resources>0)?this.resources + (int) (resources * coefficient):this.resources + (int) (resources / coefficient);
-        this.food = (food>0)?this.food + (int) (food * coefficient):this.food + (int) (food / coefficient);
-        this.happiness = Math.min(this.happiness+happiness, 100); //т.к. уровень счастье выше 100 не поднимается
+        this.money = (money > 0) ? this.money + (int) (money * coefficient) : this.money + (int) (money / coefficient);
+        this.resources = (resources > 0) ? this.resources + (int) (resources * coefficient) : this.resources + (int) (resources / coefficient);
+        this.food = (food > 0) ? this.food + (int) (food * coefficient) : this.food + (int) (food / coefficient);
+        this.happiness = Math.min(this.happiness + happiness, 100); //т.к. уровень счастье выше 100 не поднимается
 
         checkLoss();
     }
@@ -29,5 +36,13 @@ public class City {
         if (money < 0 || resources < 0 || food < 0 || happiness < 0) {
             System.exit(0); //мега-гениальный костыль
         }
+    }
+
+    public void addBuilding(Building building){
+        buildingList.add(building);
+    }
+
+    public void removeBuilding(Building building){
+        buildingList.remove(building);
     }
 }
